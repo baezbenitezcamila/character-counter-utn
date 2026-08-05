66c228a8c4c711605fbc42596e4a6be26cbda68d
camilabaezbenitez <baezbenitezcamilaa@gmail.com>
2026-08-05 08:34:26 -0700

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return  <h1>Hola!</h1>
+return (
+    <main>
+      <header>
+        <div>
+          <p></p>
+          <h1>Character Counter UTN</h1>
+        </div>
+        <button>☼</button>
+      </header>
+    </main>
+)
 }
 
-export {App}
+export { App }
