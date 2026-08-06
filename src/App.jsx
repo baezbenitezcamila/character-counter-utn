0729431e583b394c5ae58f35e8367999784769cb
camilabaezbenitez <baezbenitezcamilaa@gmail.com>
2026-08-06 15:06:09 -0700

    add: percentage

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,20 @@ const App = () => {
   
   const [excludeSpaces, setExcludeSpaces] = useState(false)
   const [limitCharacter, setLimitCharacter] = useState(false)
-  const [limitValue, setLimitValue] = useState(300)
+  const [limitValue, setLimitValue] = useState(30)
   
   const characters = excludeSpaces ? text.replace(/\s/g, ""). length : text.length
 
+  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length
+
+  const sentences = text.trim() === "" ? 0 : text.split(/[.!?]/).filter(sentence => sentence.trim() !== "").length
+
+  const readingTime = Math.ceil(words / 200)
+  
   const handleChangeTextarea = (e) => {
     const value = e.target.value
 
-
-const handleChangeInputLimit = () => {
+  const handleChangeInputLimit = () => {
     setLimitCharacter(!limitCharacter)
     const newText = text.slice(0, limitValue)
     setText(newText)
@@ -32,6 +37,30 @@ const handleChangeInputLimit = () => {
       setText(value)
     } 
   }
+
+  const cleanText = text.toLowerCase().replace(/[^a-z]/g, "")
+
+  const dictionaryLetters = {}
+  
+  cleanText.split("").forEach(letter => {
+    dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1
+    } )
+
+    
+    const letter = Object.entries(dictionaryLetters).map(dataLetter => {
+      const letter = dataLetter[0]
+      const amountLetter = dataLetter[1]
+
+      const infoToRederLetter = {
+        letterName: letter,
+        amount: amountLetter,
+        porcentaje: 0
+      }
+
+      return infoToRederLetter
+    } )
+    
+  const sortLeters = letters.sort((a,b) => b.amount - a. amount)
 
   return (
     <main>
@@ -70,7 +99,23 @@ const handleChangeInputLimit = () => {
             />  
           }
         </div>
-        <p>Cantidad de caracteres: {text.length} </p>
+        <p>Cantidad de caracteres: {characters} </p>
+        <p>Cantidad de palabras: {words} </p>
+        <p>Cantidad de oraciones: {sentences} </p>
+        <p>Tiempo aprox. de lectura: &lt;{readingTime}min </p>
+        <section>
+          <h2>Cantidad de letras</h2>
+          <article>
+            {
+              sortLetters.map (letter => (
+                <div key={letter.letterName}>
+                  <span>{letter.letterName.toUpperCase} </span>
+                  <meter min="0" max="100" value={letter.porcentaje}></meter>
+                  <span>{letter.amount} ({letter.porcentaje.toFixed(1)}%) (100%)</span>
+               </div> ))
+            }
+          </article>
+        </section>
     </main>
        
   )
